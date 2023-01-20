324d69a8e4de42e0bbcb9bf615d6dcfc7de0cec8
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-01-20 19:28:19 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,28 @@
+let link = document.querySelector('.links');
+let link_lists = document.querySelector('.link-lists');
+let menu = document.querySelector('#menu-bar');
+let closee = document.querySelector('#close-btn');
+let b = document.getElementsByTagName('body');
+let box1 = document.querySelector('.box1');
+let box2 = document.querySelector('.box2');
+
+menu.addEventListener('click',()=>{
+    link.style.flex = '1';
+    link.style.transition = '0.5s';
+    menu.style.display = 'none';
+})
+closee.addEventListener('click',()=>{
+    link.style.flex = '0';
+    link.style.transition = '0.5s';
+    menu.style.display = 'block';
+})
+box1.addEventListener('click',()=>{
+    link.style.flex = '0';
+    link.style.transition = '0.5s';
+    menu.style.display = 'block';
+})
+box2.addEventListener('click',()=>{
+    link.style.flex = '0';
+    link.style.transition = '0.5s';
+    menu.style.display = 'block';
+})
